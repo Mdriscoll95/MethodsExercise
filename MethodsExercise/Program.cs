944a313eb3080bcbfe223ae0e506f88b6aae1c35
Mdriscoll95 <michael.driscoll951@gmail.com>
2023-05-12 09:14:57 -0700

    methods exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PrintStory();
+            //int myResult = AddNums(2, 2);
+            //Console.WriteLine(myResult);
+            Console.WriteLine(AddNums(2, 2));
+            Console.WriteLine(SubtractNums(2, 2));
+            Console.WriteLine(DividNums(2, 2));
+            Console.WriteLine(MultiplyNums(2, 2));
         }
+
+        public static void PrintStory()
+        {
+            Console.WriteLine("what is your name?");
+            var username = Console.ReadLine();
+            Console.WriteLine($"Hello, {username}, what is your favorite color");
+            var color = Console.ReadLine();
+            Console.WriteLine("what is your favorite animal");
+            var animal = Console.ReadLine();
+            Console.WriteLine("what is your favorite band");
+            var band = Console.ReadLine();
+        }
+
+        public static int AddNums(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int SubtractNums(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int DividNums(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public static int MultiplyNums(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+            
+
+
+        
     }
 }
